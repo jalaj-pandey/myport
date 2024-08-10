d8bd897e5e60b15312111f430d2b1e07d123fb59
Jalaj Pandey <jalajpandey123@gmail.com>
2024-08-10 21:46:15 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,5 @@ import About from "./About.jsx";
 import Skills from "./Skills.jsx";
 import Projects from "./Projects.jsx";
 import Contact from "./Contact.jsx";
-import Certificates from "./Certificates.jsx";
 
-export { About, Skills, Projects, Contact, Certificates };
+export { About, Skills, Projects, Contact };
